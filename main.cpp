e633dffa73613ab8b85afa1396e43c0d052cd575
Max Kofler <kofler.max.dev@gmail.com>
2021-01-14 16:41:07 +0100

    Deployed too in QT5 and made it buildable

--- main.cpp
+++ main.cpp
@@ -16,14 +16,19 @@ int main(int argc, char *argv[])
     Razer razer(&w);
     razer.loadSettings();
 
-    QObject::connect(w.powerPreset, &QComboBox::currentIndexChanged, &razer, &Razer::onPowerPresetChanged);
-    QObject::connect(w.CPUboost, &QSpinBox::valueChanged, &razer, &Razer::onCPUChanged);
-    QObject::connect(w.GPUboost, &QSpinBox::valueChanged, &razer, &Razer::onGPUChanged);
-    QObject::connect(w.powerApply, &QPushButton::clicked, &razer, &Razer::onPowerApplied);
 
-    QObject::connect(w.fanPreset, &QComboBox::currentIndexChanged, &razer, &Razer::onFanPresetChanged);
-    QObject::connect(w.fanRPM, &QSlider::sliderMoved, &razer, &Razer::onFanRPMChanged);
-    QObject::connect(w.fanApply, &QPushButton::clicked, &razer, &Razer::onFanApplied);
+    //QObject::connect(w.powerPreset, SIGNAL(&QComboBox::currentIndexChanged(), &razer, &Razer::onPowerPresetChanged);
+
+
+    QObject::connect(w.powerPreset, SIGNAL(&QComboBox::currentIndexChanged(int)), &razer, SLOT(&Razer::onPowerPresetChanged(int)));
+
+    QObject::connect(w.CPUboost, SIGNAL(&QSpinBox::valueChanged(int)), &razer, SLOT(&Razer::onCPUChanged(int)));
+    QObject::connect(w.GPUboost, SIGNAL(&QSpinBox::valueChanged(int)), &razer, SLOT(&Razer::onGPUChanged(int)));
+    QObject::connect(w.powerApply, SIGNAL(&QPushButton::clicked()), &razer, SLOT(&Razer::onPowerApplied()));
+
+    QObject::connect(w.fanPreset, SIGNAL(&QComboBox::currentIndexChanged(int)), &razer, SLOT(&Razer::onFanPresetChanged(int)));
+    QObject::connect(w.fanRPM, SIGNAL(&QSlider::sliderMoved(int)), &razer, SLOT(&Razer::onFanRPMChanged(int)));
+    QObject::connect(w.fanApply, SIGNAL(&QPushButton::clicked()), &razer, SLOT(&Razer::onFanApplied()));
 
 
     //connect(w.powerPreset, &QComboBox::currentTextChanged, a, &powerChanged);
