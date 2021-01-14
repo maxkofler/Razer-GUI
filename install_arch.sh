7694355e24da5e46e248b79c68a07dabd930159a
Max Kofler <kofler.max.dev@gmail.com>
2021-01-14 17:30:59 +0100

    Added installation script for arch linux

---
+++ install_arch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+pacman --needed --noconfirm -S  make qt5-base gcc
+
+qmake
+make
+//make clean
+
+mkdir /opt/razer-gui
+cp ./Razer-GUI /opt/razer-gui/razer-gui
+chmod -R 777 /opt/razer-gui
+chmod +x /opt/razer-gui/razer-gui
+cp ./Razer-GUI.desktop /usr/share/applications/
