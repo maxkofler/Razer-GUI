6567b2372b475fbf109a13210ed307ab8044ceb3
Max Kofler <kofler.max.dev@gmail.com>
2021-01-14 16:53:07 +0100

    Fixed issue to not working signals and slots

--- main.cpp
+++ main.cpp
@@ -20,15 +20,15 @@ int main(int argc, char *argv[])
     //QObject::connect(w.powerPreset, SIGNAL(&QComboBox::currentIndexChanged(), &razer, &Razer::onPowerPresetChanged);
 
 
-    QObject::connect(w.powerPreset, SIGNAL(&QComboBox::currentIndexChanged(int)), &razer, SLOT(&Razer::onPowerPresetChanged(int)));
+    QObject::connect(w.powerPreset, SIGNAL(currentIndexChanged(int)), &razer, SLOT(onPowerPresetChanged(int)));
 
-    QObject::connect(w.CPUboost, SIGNAL(&QSpinBox::valueChanged(int)), &razer, SLOT(&Razer::onCPUChanged(int)));
-    QObject::connect(w.GPUboost, SIGNAL(&QSpinBox::valueChanged(int)), &razer, SLOT(&Razer::onGPUChanged(int)));
-    QObject::connect(w.powerApply, SIGNAL(&QPushButton::clicked()), &razer, SLOT(&Razer::onPowerApplied()));
+    QObject::connect(w.CPUboost, SIGNAL(valueChanged(int)), &razer, SLOT(onCPUChanged(int)));
+    QObject::connect(w.GPUboost, SIGNAL(valueChanged(int)), &razer, SLOT(onGPUChanged(int)));
+    QObject::connect(w.powerApply, SIGNAL(clicked()), &razer, SLOT(onPowerApplied()));
 
-    QObject::connect(w.fanPreset, SIGNAL(&QComboBox::currentIndexChanged(int)), &razer, SLOT(&Razer::onFanPresetChanged(int)));
-    QObject::connect(w.fanRPM, SIGNAL(&QSlider::sliderMoved(int)), &razer, SLOT(&Razer::onFanRPMChanged(int)));
-    QObject::connect(w.fanApply, SIGNAL(&QPushButton::clicked()), &razer, SLOT(&Razer::onFanApplied()));
+    QObject::connect(w.fanPreset, SIGNAL(currentIndexChanged(int)), &razer, SLOT(onFanPresetChanged(int)));
+    QObject::connect(w.fanRPM, SIGNAL(sliderMoved(int)), &razer, SLOT(onFanRPMChanged(int)));
+    QObject::connect(w.fanApply, SIGNAL(clicked()), &razer, SLOT(onFanApplied()));
 
 
     //connect(w.powerPreset, &QComboBox::currentTextChanged, a, &powerChanged);
